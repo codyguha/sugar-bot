b17aff9c6f4cf5c1c424fa8af4303c6af77cbbce
codyguha <aalo.guha@gmail.com>
2016-12-05 15:08:09 -0800

    heroku

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 var express = require('express')
 var app = express()
+var path = require('path');
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.sendFile(path.join(__dirname + '/broadcast.html'));
+})
+app.post('/',function(req,res){
+  var broadcast = req.body.broadcast
+  var user = req.body.user
+  console.log(req.body.broadcast)
+  res.send(user + ' says: "' + broadcast + '"')
 })
 
 app.listen(3000, function () {
