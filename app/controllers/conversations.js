4fdf0b1b9726ae044394266b45c1d5a22abbf0dc
codyguha <aalo.guha@gmail.com>
2016-12-09 11:40:39 -0800

    heorku

--- app/controllers/conversations.js
+++ app/controllers/conversations.js
@@ -9,11 +9,6 @@ module.exports = function (controller) {
     bot.reply(message, 'Hey there.')
   })
 
-  // user says anything else
-  controller.hears('(.*)', 'message_received', function (bot, message) {
-    bot.reply(message, 'you said ' + message.match[1])
-  })
-
   controller.hears(['what can I do here?'], 'message_received', function(bot, message) {
       bot.reply(message, "You can talk to me and I will respond!");
   });
@@ -21,5 +16,8 @@ module.exports = function (controller) {
   controller.hears(['help'], 'message_received', function(bot, message) {
       bot.reply(message, "type 'hello'");
   });
-
+  // user says anything else
+  controller.hears('(.*)', 'message_received', function (bot, message) {
+    bot.reply(message, 'you said ' + message.match[1])
+  })
 }
