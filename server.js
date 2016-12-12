61a55d82b8266769c05dd2eb9ed44fda0fd7bab8
codyguha <aalo.guha@gmail.com>
2016-12-12 13:50:54 -0800

    facebook login

--- server.js
+++ server.js
@@ -83,9 +83,6 @@ app.get('/login/facebook',
 app.get('/login/facebook/return',
   passport.authenticate('facebook', { failureRedirect: '/login' }),
   function(req, res) {
-    FB.api('/me/accounts', function(response){
-    console.log(response);
-    })
     res.redirect('/');
   });
 
