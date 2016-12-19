2b6b3e402838389fa9074666cb30836e8f04f321
codyguha <aalo.guha@gmail.com>
2016-12-19 12:49:33 -0800

    multichoice

--- server.js
+++ server.js
@@ -105,11 +105,6 @@ app.get('/login',
     res.render('login');
   });
 
-  app.get('/list',
-    function(req, res){
-      res.render('list', {list: ["Sugar", "Cane Sugar", "Sucrose", "Fructose", "Lactose", "Corn Syrup", "Maltodextrin", "Dextrose", "Glucose", "Evaporated Cane Juice", "Molasses", "High Fructose Corn Syrup", "Coconut Sugar", "Palm Sugar", "Rice Malt Syrup", "Malt", "Fruit Juice Concentrate", "Honey", "Maple Syrup", "Agave","Date Syrup", "Stevia", "Monk Fruit Juice", "Luo Han Guo", "Thaumatin", "Unrefined Sugar", "Mannitol", "Xylitol", "Sorbitol", "Tagatose", "Isomaltulose", "Polydextrose", "Aspartame (i.e. Equal)", "Sucralose (i.e. Splenda)", "Acesulphame Potassium", "Saccharin (i.e. Sweet ‘N Low)"]});
-    });
-
 app.get('/login/facebook',
   passport.authenticate('facebook', { scope: ['user_friends', 'manage_pages'] }));
 
