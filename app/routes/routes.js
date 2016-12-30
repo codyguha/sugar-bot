b677cd6d454c72e2766b4a0de316450cf6025135
codyguha <aalo.guha@gmail.com>
2016-12-29 16:56:09 -0800

    routes

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -3,17 +3,6 @@ var broadcaster = require('../controllers/botkit').broadcast
 
 module.exports = function (app) {
 
-  // app.get('/', function (req, res) {
-  //   res.render('home')
-  // })
-
-  // app.post('/', function (req, res) {
-  //   var text = req.body.broadcast
-  //   var broadcast=eval("("+text+")");
-  //   broadcaster(broadcast)
-  //   res.render('home')
-  // })
-
   app.get('/webhook', function (req, res) {
     // This enables subscription to the webhooks
     if (req.query['hub.mode'] === 'subscribe' && req.query['hub.verify_token'] === process.env.verify_token) {
@@ -29,22 +18,16 @@ module.exports = function (app) {
     res.send('ok')
   })
 
-  app.post('/custom-webhook', function (req, res) {
-    var text = req.body.broadcast
-    var broadcast=eval("("+text+")");
-    broadcaster(broadcast)
-    res.render('home')
-  })
-
   app.get('/list',
     function(req, res){
       res.render('list', {list: ["Sugar", "Cane Sugar", "Sucrose", "Fructose", "Lactose", "Corn Syrup", "Maltodextrin", "Dextrose", "Glucose", "Evaporated Cane Juice", "Molasses", "High Fructose Corn Syrup", "Coconut Sugar", "Palm Sugar", "Rice Malt Syrup", "Malt", "Fruit Juice Concentrate", "Honey", "Maple Syrup", "Agave","Date Syrup", "Stevia", "Monk Fruit Juice", "Luo Han Guo", "Thaumatin", "Unrefined Sugar", "Mannitol", "Xylitol", "Sorbitol", "Tagatose", "Isomaltulose", "Polydextrose", "Aspartame (i.e. Equal)", "Sucralose (i.e. Splenda)", "Acesulphame Potassium", "Saccharin (i.e. Sweet ‘N Low)"]});
   });
 
   app.post('/list',function(req,res){
+    var body = JSON.stringify(req.body, null, 4);
   	console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>ID: " + req.body.fb_id)
-    console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>FORM: " + req.body)
-    broadcaster("gotcha!", req.body.fb_id)
+    console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>FORM: " + body)
+    broadcaster(body, req.body.fb_id)
   })
 
 
