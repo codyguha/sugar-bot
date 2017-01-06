3ac1889bbe52a4c6cf4fa7a21ee2da816f11f079
codyguha <aalo.guha@gmail.com>
2017-01-06 13:15:57 -0800

    heroku

--- app/controllers/botkit.js
+++ app/controllers/botkit.js
@@ -255,5 +255,5 @@ exports.handler = handler
 exports.broadcast = broadcast
 exports.orderedlist3 = orderedlist3
 exports.orderedlist4 = orderedlist4
-exports.orderedlist3 = orderedlist5
+exports.orderedlist5 = orderedlist5
 exports.orderedlist6 = orderedlist6
