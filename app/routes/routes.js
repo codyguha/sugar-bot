8908685fdf74e587ca276914b571094deb15b628
codyguha <aalo.guha@gmail.com>
2017-01-19 15:01:51 -0800

    setup webhooks

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -108,9 +108,9 @@ module.exports = function (app) {
     var facebook_id = req.body.fb_id
     attitudinal(facebook_id)
   })
-  app.get('/products',
+  app.get('/products/:id',
     function(req, res){
-      res.render('products', {list: userslist});
+      res.render('products', {list: userslist, id: req.body.fb_id});
   });
   app.get('/personality',
     function(req, res){
@@ -147,7 +147,7 @@ module.exports = function (app) {
     // var cnslbody = JSON.stringify(req.body, null, 4);
     // console.log(cnslbody)
     var sugarperson =  req.body.person
-    res.render('people2', {sugarperson: sugarperson});
+    res.render('people2', {sugarperson: sugarperson, id: facebook_id});
   })
   app.post('/sweetenerperson',function(req,res){
     var facebook_id = req.body.fb_id
@@ -155,7 +155,7 @@ module.exports = function (app) {
     // console.log(cnslbody)
     var sugarperson =  req.body.sugarperson
     var sweetenerperson =  req.body.sweetenerperson
-    res.render('personality_sugar', {sugarperson: sugarperson, sweetenerperson: sweetenerperson, list: ["Alive", "Sporty", "Energetic", "Goal-orientated", "Assertive", "Ambitious", "Stylish", "Determined", "Serious", "Self-centered", "Quiet", "Reserved", "Modest", "Ordinary", "Tranquil", "Kind", "Friendly", "Cheerful", "Bright", "Joyful"]});
+    res.render('personality_sugar', {id: facebook_id, sugarperson: sugarperson, sweetenerperson: sweetenerperson, list: ["Alive", "Sporty", "Energetic", "Goal-orientated", "Assertive", "Ambitious", "Stylish", "Determined", "Serious", "Self-centered", "Quiet", "Reserved", "Modest", "Ordinary", "Tranquil", "Kind", "Friendly", "Cheerful", "Bright", "Joyful"]});
   })
   app.post('/personality1',function(req,res){
     var facebook_id = req.body.fb_id
@@ -163,7 +163,7 @@ module.exports = function (app) {
     // console.log(cnslbody)
     var sugarperson =  req.body.sugarperson
     var sweetenerperson =  req.body.sweetenerperson
-    res.render('personality_sweetners', {sugarperson: sugarperson, sweetenerperson: sweetenerperson, list: ["Alive", "Sporty", "Energetic", "Goal-orientated", "Assertive", "Ambitious", "Stylish", "Determined", "Serious", "Self-centered", "Quiet", "Reserved", "Modest", "Ordinary", "Tranquil", "Kind", "Friendly", "Cheerful", "Bright", "Joyful"]});
+    res.render('personality_sweetners', {id: facebook_id, sugarperson: sugarperson, sweetenerperson: sweetenerperson, list: ["Alive", "Sporty", "Energetic", "Goal-orientated", "Assertive", "Ambitious", "Stylish", "Determined", "Serious", "Self-centered", "Quiet", "Reserved", "Modest", "Ordinary", "Tranquil", "Kind", "Friendly", "Cheerful", "Bright", "Joyful"]});
   })
   app.post('/personality2',function(req,res){
     var facebook_id = req.body.fb_id
